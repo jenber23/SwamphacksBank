521127103c070825a4d8ba8f80014c6c6d113e3d
jennifer bernal <jennifer.bernal@ufl.edu>
2019-01-19 09:24:41 -0500

    IT WORKS

--- app/src/main/java/com/cash/gator/swampapp/MainActivity.java
+++ app/src/main/java/com/cash/gator/swampapp/MainActivity.java
@@ -8,45 +8,54 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+//balanceOuput, type int/number
+    //submitButton, type Button
+
+    //java code
+    int balance;
+
+    //ties to XML
+    EditText balanceOutput_;
+    Button submitButton_;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        //following 2 lines are must-have hard coded
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //actual edited code//
+
+        //type editText
+        balanceOutput_ = (EditText) findViewById(R.id.balanceOutput);
+
+        submitButton_ = (Button) findViewById(R.id.submitButton);
+
+        submitButton_.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v)
+            {
+                balance = Integer.valueOf(balanceOutput_.getText().toString());
+
             }
+
         });
+
+
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
